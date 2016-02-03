4e811f72b8d8be363518db3dde0f65c20019c415
chenyufeng <503434355@qq.com>
2016-02-03 17:50:16 +0800

    comment

--- InsertSort/main.c
+++ InsertSort/main.c
@@ -26,17 +26,21 @@ void insertSort(int *a,int n){
 
     int i,j,k;
     for (i = 1; i < n; i++) {
+        //为a[i]在前面的a[0...i-1]有序区间中找一个合适的位置
         for (j = i - 1; j >= 0; j--) {
             if (a[j] < a[i]) {
                 break;
             }
         }
 
+        //找到该位置
         if (j != i - 1) {
             int temp = a[i];
+            //将比a[i]大的数据向后移
             for (k = i - 1; k > j; k--) {
                 a[k + 1] = a[k];
             }
+            //放在正确的位置上
             a[k + 1] = temp;
         }
     }
